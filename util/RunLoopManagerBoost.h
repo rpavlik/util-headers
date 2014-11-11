730a326978181516ac4639820ac4d903d84e6674
Ryan Pavlik <ryan@sensics.com>
2014-11-11 14:58:05 -0600

    Run Loop Manager improvements.

--- util/RunLoopManagerBoost.h
+++ util/RunLoopManagerBoost.h
@@ -1,16 +1,16 @@
 /** @file
-        @brief Header
+		@brief Header
 
-        @versioninfo@
+		@versioninfo@
 
-        @date 2013
+		@date 2013
 
-        @author
-        Ryan Pavlik
-        <rpavlik@iastate.edu> and <abiryan@ryand.net>
-        http://academic.cleardefinition.com/
-        Iowa State University Virtual Reality Applications Center
-        Human-Computer Interaction Graduate Program
+		@author
+		Ryan Pavlik
+		<rpavlik@iastate.edu> and <abiryan@ryand.net>
+		http://academic.cleardefinition.com/
+		Iowa State University Virtual Reality Applications Center
+		Human-Computer Interaction Graduate Program
 
 */
 
@@ -22,7 +22,6 @@
 #ifndef INCLUDED_RunLoopManagerBoost_h_GUID_50f7b2f1_493e_4395_25ca_df2f010a34bd
 #define INCLUDED_RunLoopManagerBoost_h_GUID_50f7b2f1_493e_4395_25ca_df2f010a34bd
 
-
 // Internal Includes
 #include "RunLoopManager.h"
 
@@ -32,101 +31,80 @@
 // Standard includes
 // - none
 
-
 namespace util {
 
 class RunLoopManagerBoost : public RunLoopManagerBase {
 public:
-    RunLoopManagerBoost()
-        : currentState_(STATE_STOPPED) {}
+	RunLoopManagerBoost() : currentState_(STATE_STOPPED) {}
 
-    /// @name StartingInterface
-    /// @{
-    /*void signalStart();*/
-    void signalAndWaitForStart();
-    /// @}
-
-    /// @name LoopInterface
-    /// @{
-    void reportRunning();
-    /*bool shouldContinue();*/
-    /// @}
-
-    /// @name ShutdownInterface
-    /// @{
-    void signalShutdown();
-    void signalAndWaitForShutdown();
-    /// @}
-
-
-	/// @name LoopGuardInterface
+	/// @name StartingInterface
 	/// @{
-	void reportStateChange_(RunningState s);
-	void reportStopped_();
+	void signalStart();
+	void signalAndWaitForStart();
 	/// @}
-private:
-    boost::mutex mut_;
-    boost::condition_variable stateCond_;
-    
-	typedef boost::unique_lock<boost::mutex> Lock;
-    
-    /// protected by condition variable
-	volatile LoopGuardInterface::RunningState currentState_;
 
+	/// @name ShutdownInterface
+	/// @{
+	void signalShutdown();
+	void signalAndWaitForShutdown();
+	/// @}
+
+private:
+	void reportStateChange_(RunningState s);
+	boost::mutex mut_;
+	boost::condition_variable stateCond_;
+
+	typedef boost::unique_lock<boost::mutex> Lock;
+
+	/// protected by condition variable
+	volatile RunningState currentState_;
 };
 
-inline void RunLoopManagerBoost::reportRunning() {
-	reportStateChange_(LoopGuardInterface::STATE_RUNNING);
+inline void RunLoopManagerBoost::signalStart() {
+	{
+		Lock condGuard(mut_);
+		setShouldStop_(false);
+	}
 }
 
-inline void RunLoopManagerBoost::reportStateChange_(RunLoopManagerBase::RunningState s) {
-    {
-        Lock condGuard(mut_);
-        currentState_ = s;
-    }
+inline void
+RunLoopManagerBoost::reportStateChange_(RunLoopManagerBase::RunningState s) {
+	{
+		Lock condGuard(mut_);
+		currentState_ = s;
+	}
 	stateCond_.notify_all();
 }
 
-
-inline void RunLoopManagerBoost::reportStopped_() {
-    {
-        Lock condGuard(mut_);
-        setShouldStop_(false);
-    }
-    reportStateChange_(STATE_STOPPED);
-}
-
 inline void RunLoopManagerBoost::signalAndWaitForStart() {
-    signalStart();
-    {
+	signalStart();
+	{
 		Lock condGuard(mut_);
-        while (currentState_ != STATE_RUNNING) {
+		while (currentState_ != STATE_RUNNING) {
 			stateCond_.wait(condGuard);
-        }
-    }
+		}
+	}
 }
 
 inline void RunLoopManagerBoost::signalShutdown() {
 	Lock condGuard(mut_);
-    if (currentState_ != STATE_STOPPED) {
-        setShouldStop_(true);
-    }
+	if (currentState_ != STATE_STOPPED) {
+		setShouldStop_(true);
+	}
 }
-
 
 inline void RunLoopManagerBoost::signalAndWaitForShutdown() {
 	Lock condGuard(mut_);
 
-    if (currentState_ != STATE_STOPPED) {
-        setShouldStop_(true);
-    }
+	if (currentState_ != STATE_STOPPED) {
+		setShouldStop_(true);
+	}
 
-    do {
+	while (currentState_ != STATE_STOPPED) {
 		stateCond_.wait(condGuard);
-    } while (currentState_ != STATE_STOPPED);
+	}
 }
 
 } // end of namespace util
 
 #endif // INCLUDED_RunLoopManagerBoost_h_GUID_50f7b2f1_493e_4395_25ca_df2f010a34bd
-
