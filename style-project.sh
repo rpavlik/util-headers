ac102eec4967b357a1b606ddb7f4b0dd5aecee37
Ryan Pavlik <ryan@sensics.com>
2015-05-14 14:11:15 -0500

    Fix astyle script.

--- style-project.sh
+++ style-project.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 PROJECT=$(cd $(dirname $0) && pwd)
+export ARTISTIC_STYLE_OPTIONS=${PROJECT}/astylerc
 astyle -n --recursive "${PROJECT}/util/*.h" --recursive "${PROJECT}/tests/*.cpp"
 
